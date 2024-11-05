476ca4f503393b58b10798f061e4a248e9f0ca40
Ivan Bosnic <ivan.bosnic@rimac-technology.com>
2024-11-05 17:34:36 +0100

    Lifted userInput state from InvestmentInputs to App, and forwarded it to Results

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,26 @@ import { useState } from "react";
 
 import InvestmentInputs from "./components/InvestmentInputs"
 import InvestmentResults from "./components/InvestmentResults"
-import { calculateInvestmentResults } from './util/investment'
-
-const ANNUAL_DATA_PRESET = [
-  { year: 1, interest: 500, valueEndOfYear: 10500, annualInvestment: 10000 },
-  { year: 2, interest: 1025, valueEndOfYear: 21525, annualInvestment: 10000 },
-  // Add more yearly data as needed
-];
 
 
 function App() {
-  const [annualData, setAnnualData] = useState(ANNUAL_DATA_PRESET);
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 8,
+    duration: 10
+  });
 
-  function handleInputChange(userInput) {
-    const calculatedAnnualData = calculateInvestmentResults(userInput);
-    console.log(calculatedAnnualData);
-    setAnnualData(calculatedAnnualData);
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => { return { ...prevUserInput, [inputIdentifier]: newValue }; });
   }
 
   return (
     <>
       <div>
-        <InvestmentInputs onAnyInputChange={handleInputChange} />
+        <InvestmentInputs onChange={handleChange} userInput={userInput} />
       </div>
-      <InvestmentResults annualData={annualData} />
+      <InvestmentResults userInput={userInput} />
     </>
   )
 }
--- src/components/InvestmentInputs.jsx
+++ src/components/InvestmentInputs.jsx
@@ -1,34 +1,29 @@
 import InputGroup from "./InputGroup";
-import { useState } from "react";
 
-export default function InvestmentInputs({ onAnyInputChange }) {
-    const [userInput, setUserInput] = useState({
-        initialInvestment: 10000,
-        annualInvestment: 1200,
-        expectedReturn: 8,
-        duration: 10
-    });
-
-    function handleChange(inputIdentifier, newValue) {
-        setUserInput(prevUserInput => { return { ...prevUserInput, [inputIdentifier]: newValue }; });
-        onAnyInputChange(userInput);
-    }
-
+export default function InvestmentInputs({ onChange, userInput }) {
     return (
         <section id="user-input">
             <InputGroup inputs={[
                 {
-                    inputName: "Početni ulog", inputValue: userInput.initialInvestment, onInputChange: (value) => handleChange('initialInvestment', value)
+                    inputName: "Početni ulog",
+                    inputValue: userInput.initialInvestment,
+                    onInputChange: (value) => onChange('initialInvestment', value)
                 },
                 {
-                    inputName: "Godišnje ulaganje", inputValue: userInput.annualInvestment, onInputChange: (value) => handleChange('annualInvestment', value)
+                    inputName: "Godišnje ulaganje",
+                    inputValue: userInput.annualInvestment,
+                    onInputChange: (value) => onChange('annualInvestment', value)
                 }]} />
             <InputGroup inputs={[
                 {
-                    inputName: "Očekivani povrat (%)", inputValue: userInput.expectedReturn, onInputChange: (value) => handleChange('expectedReturn', value)
+                    inputName: "Očekivani povrat (%)",
+                    inputValue: userInput.expectedReturn,
+                    onInputChange: (value) => onChange('expectedReturn', value)
                 },
                 {
-                    inputName: "Trajanje (godina)", inputValue: userInput.duration, onInputChange: (value) => handleChange('duration', value)
+                    inputName: "Trajanje (godina)",
+                    inputValue: userInput.duration,
+                    onInputChange: (value) => onChange('duration', value)
                 }]} />
         </section>
     );
